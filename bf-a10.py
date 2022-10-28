1af66a6d266aa01970d4bee7da9d4d98ea6d6920
Diego Cueva LSU <diegocuevacastro@f4-0f-24-35-06-0c.wlan.lsu.edu>
2022-10-28 18:30:30 -0500

    update assigment 10

--- bf-a10.py
+++ bf-a10.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+from asyncore import write
+from re import L
+
+
 def word_counting(var1):
     with open(var1, 'r') as f:
         words = f.read().lower().replace(",","").replace(".","").split(" ")
@@ -8,14 +12,32 @@ def word_counting(var1):
         for word in most_common:
             print(word[0], "\t", word[1])
 
-def Loopdedoo():
-    with open("numbers.txt", 'r') as number:
-        with open("output.txt", 'w') as output:
+def Loopdedoo(var2):
+    l = 10
+    with open(var2, 'r') as numbers:
+        for n in numbers:
+            if int(n) <= l:
+                print(n)
+                with open("numbers-" + str(l) + ".txt", "a") as file:
+                    file.write(n)
+            else:
+                l = l + 10
+
+def Megacopy():
+    os.mkdir("many-files-updated")
+    for f in os.listdir("many-files"):
+        origin = os.path.join("/home/runner/Assignment-10-dcueva/many-files",f)
+        destination = os.path.join("/home/runner/Assignment-10-dcueva/many-files-updated",os.path.splitext(os.path.basename(f))[0]+".info")
+        shutil.copy2(origin,destination)
 
 def main():
     word_counting("stars.txt")
-    Loopdedoo()
+    #Loopdedoo("numbers.txt")
+    Megacopy()
+
 
 if __name__ == '__main__':
     from collections import Counter
+    import os
+    import shutil
     main()
