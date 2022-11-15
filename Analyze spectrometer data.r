c2079d719ec3a4b6963dbc6da57d56a385c55f6f
Diego Cueva <diegoacuevac@gmail.com>
2022-11-14 21:58:04 -0600

    Starting R-color script

---
+++ Analyze spectrometer data.r
@@ -0,0 +1,9 @@
+#####Analyze spectrometer data#####
+
+install.packages(
+    c("installr", "ggplot2", "plyr", "readr", "GGally", "dplyr", "mlbench", "pavo", "colourvision", "tidyr", "tidyverse", "rgl", "scales", 
+    "RColorBrewer", "viridis", "paletteer", "ggtext", "magick")
+) #Install packages, delete the first "#" to install packages
+
+installed.packages() #Check if packages are installed
+
---
+++ Radseq Thraupis.bat
@@ -0,0 +1,3 @@
+"""Radseq Thraupis"""
+::Uploaad data:
+scp -r my_computer/path/to/data/ path/to/remote/server
