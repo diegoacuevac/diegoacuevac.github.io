a7ea18c0b2b14b33e7a2ab6ab90081e0ad8ff875
Diego Cueva <diegoacuevac@gmail.com>
2022-11-19 22:30:57 -0600

    code spectrometer data graph

--- Analyze spectrometer data.r
+++ Analyze spectrometer data.r
@@ -1,14 +1,16 @@
 #####Analyze spectrometer data#####
 
-#install.packages(c("installr", "ggplot2", "plyr", "readr", "GGally", "dplyr", "mlbench", "pavo", "colourvision", "tidyr", "tidyverse", "rgl", "scales", 
-#    "RColorBrewer", "viridis", "paletteer", "ggtext", "magick")) #Install packages, delete the first "#" to install packages
+install.packages(c("installr", "ggplot2", "plyr", "readr", "GGally", "dplyr", "mlbench", "pavo", "colourvision", "tidyr", "tidyverse", "rgl", "scales", 
+    "RColorBrewer", "viridis", "paletteer", "ggtext", "magick")) #Install packages, delete the first "#" to install packages
+
+install.packages("pavo")
 
 installed.packages() #Check if packages are installed
 
+library(pavo)
 library(installr)
 library(dplyr)
 library(tidyverse) #pipe %>%  is included in this package
-library(pavo)
 library(ggplot2)
 library(tidyr)
 library(GGally)
@@ -19,12 +21,23 @@ library(viridis) #simple and versatile go-to option for coloring graphs
 library(ggtext) #important package for formatting ggtitle and text
 
 grallaria = getspec(
-    where = "/Users/diegocueva/Library/CloudStorage/GoogleDrive-dacuevac@unal.edu.co/My Drive/Manacus/spectro/grallaria2/",
+    where = "/Users/diegocueva/Library/CloudStorage/GoogleDrive-dacuevac@unal.edu.co/My Drive/Manacus/spectro/throat/",
     ext = "txt",
     lim = c(300, 700),
     decimal = "."
 )
-?getspec()
-plot(grallaria)
-png("test.png", 300, 600)
-getwd()
+head(grallaria)
+
+bygralla <- gsub("-.*$","",names(grallaria))
+
+head(bygralla)
+png(filename="grallaria.png", width=3000, height=3000, units="px", res=300)
+aggplot(grallaria, bygralla,
+    shadecol = c("#000000","#ff00bf","#00c1b4"), alpha = 0.17,
+    lcol = c("#000000","#ff00bf","#00c1b4"),
+    lwd = 1,
+    ylim = c(0, 40),
+    clim = c(300, 700),
+    xlab=expression(lambda * " (nm)"),
+) 
+dev.off()
